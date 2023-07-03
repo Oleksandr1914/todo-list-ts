dbc9c3ce287417d1c1ccfa01a7da628fe7609266
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-07-03 22:19:59 +0300

    added createAsyncThunk in redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
-import { addTodo } from './store/todoSlice';
-import { useAppDispatch } from './hook';
+import { fetchTodos, addNewTodo } from './store/todoSlice';
+import { useAppDispatch, useAppSelector } from './hook';
 import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
 
 
 function App() {
   const [text, setText] = useState('');
+  const{loading, error}= useAppSelector(state=> state.todos)
   const dispatch = useAppDispatch();
 
   const handleAction = () => {
     if(text.trim().length) {
-      dispatch(addTodo(text));
+      dispatch(addNewTodo(text));
       setText('');
     }
   }
+
+  useEffect(() => {
+    dispatch(fetchTodos())
+  },[dispatch])
 
   return (
     <div className="w-full h-screen bg-slate-300 flex flex-col gap-3">
@@ -24,6 +29,8 @@ function App() {
         updateText={setText}
         handleAction={handleAction}
       />
+       {loading  && <h2>Loading...</h2>}
+      {error &&  <h2>An error occured: {error}</h2>}
       <TodoList />
     </div>
   );
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,5 @@
 import { useAppDispatch } from '../hook';
-import {toggleComplete, removeTodo} from '../store/todoSlice';
+import {toggleStatus, deleteTodo} from '../store/todoSlice';
 
 interface TodoItemProps {
     id: string,
@@ -15,13 +15,14 @@ const TodoItem: React.FC<TodoItemProps> = ({ id, title, completed }) => {
       <input
         type='checkbox'
         checked={completed}
-        onChange={() => dispatch(toggleComplete( id))}
+        onChange={() => dispatch(toggleStatus( id))}
       />
       <span  className="text-lg">{title}</span>
-      <button type="button" onClick={() => dispatch(removeTodo(id))} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center">delete</button>
+      <button type="button" onClick={() => dispatch(deleteTodo(id))} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center">delete</button>
 
     </li>
   );
 };
 
 export default TodoItem;
+
